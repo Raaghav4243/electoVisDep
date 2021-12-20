bcf90e3f48d90a91514d35106a4dfedf3cbd18bd
Raaghav <raaghav.4243@gmail.com>
2021-12-20 17:32:45 +0530

    Updates

---
+++ precache-manifest.77a2bf428e7298d697e8de26c097f915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf5ebf619440d2e4bf03fb1a03a558c",
+    "url": "/electoVisDep/index.html"
+  },
+  {
+    "revision": "6ab547f8426944698516",
+    "url": "/electoVisDep/static/css/main.e4738386.chunk.css"
+  },
+  {
+    "revision": "6555727d10a339872aa0",
+    "url": "/electoVisDep/static/js/2.aaef5698.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/electoVisDep/static/js/2.aaef5698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab547f8426944698516",
+    "url": "/electoVisDep/static/js/main.61b26b19.chunk.js"
+  },
+  {
+    "revision": "7d43d901141459267a79",
+    "url": "/electoVisDep/static/js/runtime-main.1deb3197.js"
+  }
+]);
